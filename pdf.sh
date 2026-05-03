480cca788c30c4d4c2a8524ae15bd8c785397658
muadhbelkacem <muadhbelkacem@gmail.com>
2026-05-03 22:58:09 +0100

    chapter 2 jfkdjflks

--- pdf.sh
+++ pdf.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Pdf"
-pdflatex -output-directory=out main.tex; pdflatex -output-directory=out main.tex;
+pdflatex -output-directory=out main.tex
+pdflatex -output-directory=out main.tex
