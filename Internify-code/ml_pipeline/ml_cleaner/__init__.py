c7fb80942ffa982e45282cbd3602758873bf3c92
muadhbelkacem <muadhbelkacem@gmail.com>
2026-06-01 13:39:00 +0100

    jj

--- Internify-code/ml_pipeline/ml_cleaner/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Internify local ML cleaning and enrichment package."""
-
-from .config import CleanerConfig, DEFAULT_CONFIG
-from .pipeline import MLCleanerPipeline
-
-__all__ = ["CleanerConfig", "DEFAULT_CONFIG", "MLCleanerPipeline"]
--- pre.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Presentation"
-pdflatex -output-directory=out presentation.tex
-bibtex out/presentation
-pdflatex -output-directory=out presentation.tex
-pdflatex -output-directory=out presentation.tex
